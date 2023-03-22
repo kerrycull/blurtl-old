2168f2c77fb332f9fe4de7e810bec52588df813d
kerrycull <kerrywcull@hotmail.com>
2023-03-22 23:38:11 +0000

    trying keep awake

--- src/components/LatestArticles.js
+++ src/components/LatestArticles.js
@@ -9,8 +9,10 @@ import {
   doc,
 } from "firebase/firestore";
 import { db } from "../firebase";
+import { useKeepAwake } from "@sayem314/react-native-keep-awake";
 
 function LatestArticles() {
+  useKeepAwake();
   const [posts, setPosts] = useState([]);
   const [newPosts, setNewPosts] = useState([]);
   const [postDisplay, setPostDisplay] = useState([]);
@@ -78,7 +80,7 @@ function LatestArticles() {
     console.log("articleGrabber");
     const intervalId = setInterval(() => {
       articleGrabber();
-    }, 18000);
+    }, 180000);
 
     return () => clearInterval(intervalId);
   }, []);
