02009914e5d034d8327585cdce3086ff0771e4e7
kerrycull <kerrywcull@hotmail.com>
2023-04-24 15:13:55 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,15 @@ import "../App.css";
 import Modal from "./Modal/Modal.js";
 import Footer from "./Footer.js";
 import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="top">
       <div className="navigation">
-      <img className="logo" src={logo} width='30px' alt="blurtl"/>
+        <Link to="/">
+          <img className="logo" src={logo} width='30px' alt="blurtl"/>
+        </Link>
         <nav>
           <ul>
             <li>
