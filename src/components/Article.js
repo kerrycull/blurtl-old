d2681c4c43f165a9db7099ab2e3888c757078f99
kerrycull <kerrywcull@hotmail.com>
2023-04-29 11:17:34 +0100

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback} from "react";
+import React, { useEffect, useState} from "react";
 import "../Article.css";
 import axios from "axios";
 import { auth } from "./Modal/firebase.js";
